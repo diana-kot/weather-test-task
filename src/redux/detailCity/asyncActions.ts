54cc9ab748f0f5cef1092e1000a9b3c4ae0728b2
diana-kot <57452102+diana-kot@users.noreply.github.com>
2023-11-28 13:09:09 +0400

    fix consol

--- src/redux/detailCity/asyncActions.ts
+++ src/redux/detailCity/asyncActions.ts
@@ -7,12 +7,10 @@ import { API_KEY, SERVER_URL_API } from 'constants/config';
 export const fetchDetailCity = createAsyncThunk<ICity, any>(
     'cities/fetchDetailCity',
     async (id, { rejectWithValue }) => {
-        console.log('id', id)
-        
+    
         const arrId = id.split(' ');
         const idNew = arrId[arrId.length - 1];
-        console.log('idNew', idNew)
-
+    
         const url = `${SERVER_URL_API}/weather?q=${idNew}&appid=${API_KEY}&units=metric&lang=ru`;
 
         try {
